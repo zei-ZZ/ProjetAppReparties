2fa780e481f71690bf705e45169b0bc4fe1b6419
WindyJasmin <yassmine.riahi@insat.ucar.tn>
2023-05-12 08:20:21 +0100

    I added the GUI and made changes in the ConnexionClient.java

---
+++ ClientInterface.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ClientInterface extends JFrame implements ActionListener {
+    private Connexion connexion;
+    private JTextField nameField;
+    private JButton addButton;
+
+    public ClientInterface(Connexion connexion) {
+        this.connexion = connexion;
+        setTitle("Ajouter un client");
+        setSize(300, 150);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // Créer les champs de texte et le bouton
+        nameField = new JTextField(20);
+        addButton = new JButton("Ajouter");
+        addButton.addActionListener(this);
+
+        // Utiliser le gestionnaire de mise en page GridLayout
+        setLayout(new GridLayout(3, 2));
+        add(new JLabel("Nom :"));
+        add(nameField);
+        add(addButton);
+    }
+
+    
+        public void actionPerformed(ActionEvent e) {
+            String clients = ""; // Declare clients variable outside of try block
+            
+            if (e.getSource() == addButton) {
+                // Récupérer les valeurs des champs de texte
+                String name = nameField.getText();
+                
+        
+                // Ajouter le client à la liste
+                try {
+                    clients = connexion.addConnected(name); // Assign value to clients variable
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(this, "Erreur lors de l'ajout du client.");
+                    return;
+                }
+                
+                JOptionPane.showMessageDialog(this, "Client ajouté.");
+                JOptionPane.showMessageDialog(this, "Liste des clients : " + clients);
+            }
+        }
+        
+    }
+
--- ConnexionClient.java
+++ ConnexionClient.java
@@ -8,7 +8,22 @@ public class ConnexionClient {
         }
 
         String name = args[0];
+/* if you want to use the GUI 
+ * try {
+    ConnexionInterface c = (ConnexionInterface) Naming.lookup("rmi://localhost/ConnexionService");
+    String message = "Adding " + name + " to connected clients...\n";
+    message += c.addConnected(name) + "\n";
+    int connected = c.getConnected();
+    message += "Number of connected clients: " + connected;
+    JOptionPane.showMessageDialog(null, message);
+} catch (Exception e) {
+    JOptionPane.showMessageDialog(null, "Error while connecting to server: " + e.getMessage());
+}
 
+ */
+
+
+        
         try {
             ConnexionInterface c = (ConnexionInterface) Naming.lookup("rmi://localhost/ConnexionService");
             System.out.println("Adding " + name + " to connected clients...");
